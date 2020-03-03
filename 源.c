b4d1edff9954e04ea8cc115463aa55a31cbb8bbc
irismic <61542752+irismic@users.noreply.github.com>
2020-03-03 17:18:14 +0800

    初始框架

---
+++ 源.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void Menu();//主菜单
+void input();//输入生日，测定时间
+void calculate();//通过生日测定时间计算周期
+void put();//输出计算结果
+int main() {
+	Meun();
+	input();
+	calculate();
+	put();
+	return 0;
+}
